4cf2dfb4059b482fb562ebd0d35149f7887a2111
Lukyanov Svyatoslav <svyat.lknv@yandex.ru>
2022-12-13 21:45:48 +0300

    Move ViewModelFactory to appropriate classes

--- app/src/main/java/com/example/greatweek/presentation/viewmodel/RoleTabViewModel.kt
+++ app/src/main/java/com/example/greatweek/presentation/viewmodel/RoleTabViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.greatweek.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.greatweek.domain.model.Role
 import com.example.greatweek.domain.usecase.role.AddRoleUseCase
 import com.example.greatweek.domain.usecase.role.DeleteRoleUseCase
 import com.example.greatweek.domain.usecase.role.GetRolesUseCase
 import com.example.greatweek.domain.usecase.role.RenameRoleUseCase
 import kotlinx.coroutines.flow.Flow
+import java.lang.IllegalArgumentException
 
 class RoleTabViewModel(
     private val getRolesUseCase: GetRolesUseCase,
@@ -19,5 +21,21 @@ class RoleTabViewModel(
 
     fun deleteRole(roleId: Int) {
         deleteRoleUseCase.execute(roleId = roleId)
+    }
+}
+
+class RoleTabViewModelFactory(
+    private val getRolesUseCase: GetRolesUseCase,
+    private val deleteRoleUseCase: DeleteRoleUseCase,
+) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RoleTabViewModel::class.java)) {
+            return RoleTabViewModel(
+                getRolesUseCase,
+                deleteRoleUseCase,
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
--- app/src/main/java/com/example/greatweek/presentation/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/com/example/greatweek/presentation/viewmodel/ScheduleViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.greatweek.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.greatweek.domain.model.WeekDay
 import com.example.greatweek.domain.usecase.goal.GetWeekUseCase
 import kotlinx.coroutines.flow.Flow
@@ -11,5 +12,19 @@ class ScheduleViewModel(
 
     fun getWeek(): Flow<List<WeekDay>> {
         return getWeekUseCase.execute()
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class ScheduleViewModelFactory(
+    private val getWeekUseCase: GetWeekUseCase
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ScheduleViewModel::class.java)) {
+            return ScheduleViewModel(
+                getWeekUseCase
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
