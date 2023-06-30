56f5992b26af1719ae72e4e751e2392ad808bc31
Svyatoslav <svyat.lknv@gmail.com>
2023-06-30 23:46:15 +0300

    Switch from Nav Component to Cicerone

--- app/src/main/java/com/example/greatweek/app/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/greatweek/app/presentation/view/MainActivity.kt
@@ -6,33 +6,46 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.greatweek.R
 import com.example.greatweek.app.GreatWeekApplication
+import com.example.greatweek.app.presentation.navigation.Screens
 import com.example.greatweek.app.presentation.viewmodel.ScheduleViewModel
 import com.example.greatweek.app.presentation.viewmodel.ScheduleViewModelFactory
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.androidx.AppNavigator
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var navController: NavController
+    @Inject lateinit var navigatorHolder: NavigatorHolder
+    @Inject lateinit var router: Router
+    private val navigator = AppNavigator(this, R.id.mainContainer)
 
-    @Inject
-    lateinit var scheduleViewModelFactory: ScheduleViewModelFactory
+    @Inject lateinit var scheduleViewModelFactory: ScheduleViewModelFactory
     private lateinit var scheduleViewModel: ScheduleViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as GreatWeekApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
+        scheduleViewModel = ViewModelProvider(this, scheduleViewModelFactory)[ScheduleViewModel::class.java]
 
-        scheduleViewModel =
-            ViewModelProvider(this, scheduleViewModelFactory)[ScheduleViewModel::class.java]
+        if (savedInstanceState == null) {
+            router.replaceScreen(Screens.Schedule())
+        }
+    }
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+    override fun onPause() {
+        navigatorHolder.removeNavigator()
+        super.onPause()
+    }
 
+    override fun onBackPressed() {
+        router.exit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
